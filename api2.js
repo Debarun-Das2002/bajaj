2fb9ec5f69fe06c10eb02daecafd0a850178374a
Debarun Das <67622252+Debarun-Das2002@users.noreply.github.com>
2024-03-18 15:11:01 +0530

    Update api2.js

--- api2.js
+++ api2.js
@@ -2,9 +2,9 @@ const investInBajajFinserv = async (accountNumber, currentPrice, githubRepoLink)
   try {
     const response = await axios.post('https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks', {
       company: 'Bajaj Finserv',
-      currentPrice: 1578.15,
+      currentPrice: 1500,
       accountNumber:"BFHL0018655",
-      githubRepoLink:""
+      githubRepoLink:"https://github.com/Debarun-Das2002/bajaj"
     }, {
       headers: {
         'content-type': 'application/json',
