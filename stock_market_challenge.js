4d01ee2b04fce8a410c57dd49258675b9d331aba
Debarun Das <67622252+Debarun-Das2002@users.noreply.github.com>
2024-03-18 15:49:49 +0530

    Create stock_market_challenge.js

---
+++ stock_market_challenge.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+// Create Investment Account
+async function createAccount() {
+  try {
+    const response = await axios.post('https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/createAccount', {
+      name: 'Debarun Das',
+      email: 'debarun0411.be21@chitkara.edu.in',
+      rollNumber:2110990411,
+      phone:7896880963
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log('Account Created:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error Creating Account:', error);
+  }
+}
+
+// Buy Stocks
+async function buyStocks(accountNumber, githubRepoLink) {
+  try {
+    const response = await axios.post('https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks', {
+      company: 'Bajaj Finserv',
+      currentPrice: 1500,
+      accountNumber: "BFHL0018655",
+      githubRepoLink:"https://github.com/Debarun-Das2002/bajaj"
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'bfhl-auth': 2110990411
+      }
+    });
+
+    console.log('Stocks Bought:', response.data);
+  } catch (error) {
+    console.error('Error Buying Stocks:', error);
+  }
+}
+
+// Main Function
+async function main() {
+  const account = await createAccount();
+  const accountNumber = account.accountNumber;
+  const githubRepoLink = 'https://github.com/your_username/your_repo';
+
+  buyStocks(accountNumber, githubRepoLink);
+}
+
+main();
