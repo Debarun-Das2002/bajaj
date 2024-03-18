8acd3fe4ac69bdebb6583dc37cd315d19652870a
Debarun Das <67622252+Debarun-Das2002@users.noreply.github.com>
2024-03-18 14:52:40 +0530

    Update api1.js

--- api1.js
+++ api1.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 const createAccount = async (name, email, rollNumber, phone) => {
   try {
     const response = await axios.post('https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/createAccount', {
-      name,
-      email,
-      rollNumber,
-      phone
+      name:"Bajaj Finserv",
+      email:"debarun0411.be21@chitkara.edu.in",
+      rollNumber:2110990411,
+      phone:7896880963
     }, {
       headers: {
         'Content-Type': 'application/json'
